1c130e9b2ee7227d307f82b4bbf1285af5aa7f82
narvis2 <narvis2@douzone.com>
2022-10-14 14:15:47 +0900

    필요한 라이브러리 추가
    ## 🍎 필요한 라이브러리 추가
    ### 🚩androidx.hilt:hilt-navigation-compose:1.0.0
    - Compose 에서 보다 편하게 Hilt를 이용해 ViewModel 을 생성하기 위해 추가
    ``` gradle
    implementation "androidx.hilt:hilt-navigation-compose:1.0.0"
    ```
    
    ### 🚩io.coil-kt:coil-compose:1.4.0
    - Compose 에서 보다 편하게 이미지를 handling 하기 위해 추가

--- app/src/main/java/com/example/moviesearchapp/view/MainActivity.kt
+++ app/src/main/java/com/example/moviesearchapp/view/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.moviesearchapp.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
 import com.example.moviesearchapp.ui.theme.ComposeMovieSearchAppTheme
 import com.example.moviesearchapp.view.navigation.MovieNavigation
@@ -14,28 +14,20 @@ import kotlinx.coroutines.CoroutineScope
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val mainViewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeMovieSearchAppTheme {
-                MovieApp(mainViewModel = mainViewModel, coroutineScope = lifecycleScope)
+                MovieApp(coroutineScope = lifecycleScope)
             }
         }
     }
 }
 
 @Composable
-fun MovieApp(mainViewModel: MainViewModel, coroutineScope: CoroutineScope) {
-    /**
-     * state holder -> MovieApp()
-     * 실제 UI를 그리는 Composable -> HomeScreen()
-     * --------------- 설명 ---------------------
-     * UI를 직접 그리는 HomeScreen Composable 에는 ViewModel 을 직접 주입하지 않고,
-     * state holder 를 담당하는 Composable 을 한 단계 거치도록 함
-     */
-    MovieNavigation(mainViewModel = mainViewModel, coroutineScope = coroutineScope)
+fun MovieApp(coroutineScope: CoroutineScope) {
+    val viewModel: MainViewModel = hiltViewModel()
+    MovieNavigation(mainViewModel = viewModel, coroutineScope = coroutineScope)
 }
 
 @Preview(showBackground = true)
