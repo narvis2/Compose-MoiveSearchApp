0bbba1c22f36c013abdc06ae0f2a7b6898d5f057
narvis2 <narvis2@douzone.com>
2022-10-14 17:16:08 +0900

    Compose Navigation + BottomSheetDialog 를 위한 라이브러리 의존성 추가
    ## Compose Navigation + BottomSheetDialog 를 위한 라이브러리 의존성 추가
    - om.google.accompanist:accompanist-navigation-material 라이브러리 사용
    ``` gradle
    implementation "com.google.accompanist:accompanist-navigation-material:0.25.1"
    ```
    - 기존의 `NavHost` 를 `ModalBottomSheetLayout` 로 감싸고 `bottomSheet()` 함수를 사용
    ``` kotlin
        val bottomSheetNavigator = rememberBottomSheetNavigator()
        val navController = rememberNavController(bottomSheetNavigator)
    
        return ModalBottomSheetLayout(
            bottomSheetNavigator = bottomSheetNavigator,
            sheetShape = RoundedCornerShape(topEnd = 16.dp, topStart = 16.dp),
        ) {
            NavHost(navController = navController, startDestination = NavigationType.HOME_SCREEN.name) {
                composable(route = NavigationType.HOME_SCREEN.name) {
                    HomeScreen(
                        navController = navController,
                        scaffoldState = scaffoldState,
                        searchQuery = searchQuery.value,
                        setSearchQuery = mainViewModel::setSearchQuery,
                        onClearQuery = mainViewModel::onClearQuery
                    )
                }
    
                bottomSheet(route= "") {
                    // TODO: Composable 넣기
                }
            }
        }
    ```

--- app/src/main/java/com/example/moviesearchapp/view/navigation/MovieNavigation.kt
+++ app/src/main/java/com/example/moviesearchapp/view/navigation/MovieNavigation.kt
@@ -1,34 +1,51 @@
 package com.example.moviesearchapp.view.navigation
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.moviesearchapp.view.MainViewModel
 import com.example.moviesearchapp.view.screen.home.HomeScreen
+import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
+import com.google.accompanist.navigation.material.ModalBottomSheetLayout
+import com.google.accompanist.navigation.material.bottomSheet
+import com.google.accompanist.navigation.material.rememberBottomSheetNavigator
 import kotlinx.coroutines.CoroutineScope
 
+@OptIn(ExperimentalMaterialNavigationApi::class)
 @Composable
 fun MovieNavigation(mainViewModel: MainViewModel, coroutineScope: CoroutineScope) {
-    val navController = rememberNavController()
-
     // Snack Bar
     val scaffoldState = rememberScaffoldState()
 
     // Home
     val searchQuery = mainViewModel.searchQuery.collectAsState()
 
-    return NavHost(navController = navController, startDestination = NavigationType.HOME_SCREEN.name) {
-        composable(route = NavigationType.HOME_SCREEN.name) {
-            HomeScreen(
-                navController = navController,
-                scaffoldState = scaffoldState,
-                searchQuery = searchQuery.value,
-                setSearchQuery = mainViewModel::setSearchQuery,
-                onClearQuery = mainViewModel::onClearQuery
-            )
+    val bottomSheetNavigator = rememberBottomSheetNavigator()
+    val navController = rememberNavController(bottomSheetNavigator)
+
+    return ModalBottomSheetLayout(
+        bottomSheetNavigator = bottomSheetNavigator,
+        sheetShape = RoundedCornerShape(topEnd = 16.dp, topStart = 16.dp),
+    ) {
+        NavHost(navController = navController, startDestination = NavigationType.HOME_SCREEN.name) {
+            composable(route = NavigationType.HOME_SCREEN.name) {
+                HomeScreen(
+                    navController = navController,
+                    scaffoldState = scaffoldState,
+                    searchQuery = searchQuery.value,
+                    setSearchQuery = mainViewModel::setSearchQuery,
+                    onClearQuery = mainViewModel::onClearQuery
+                )
+            }
+
+            bottomSheet(route= "") {
+                // TODO: Composable 넣기
+            }
         }
     }
 }
