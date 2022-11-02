6e563c04fcefab1d63e58073d499d60839baca89
narvis2 <narvis2@douzone.com>
2022-11-02 17:56:39 +0900

    🍎 Network Dialog Custom 처리
    ### 🍎 Network Dialog Custom 처리
    - AlertDialog -> Dialog 로 변경하여 Custom 처리

--- app/src/main/java/com/example/moviesearchapp/view/widgets/NetworkOfflineDialog.kt
+++ app/src/main/java/com/example/moviesearchapp/view/widgets/NetworkOfflineDialog.kt
@@ -1,26 +1,67 @@
 package com.example.moviesearchapp.view.widgets
 
-import androidx.compose.material.AlertDialog
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.example.moviesearchapp.R
 import com.example.moviesearchapp.view.network.NetworkState
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun NetworkOfflineDialog(
-    networkState: NetworkState,
-    onRetry: () -> Unit
+    networkState: NetworkState, onRetry: () -> Unit
 ) {
     if (networkState is NetworkState.NotConnected) {
-        AlertDialog(
-            onDismissRequest = {},
-            title = { Text(text = "인터넷이 연결되지 않았습니다.") },
-            text = { Text(text = "인터넷 연결을 확인하신 후 재시도 버튼을 눌러주세요.") },
-            confirmButton = {
-                TextButton(onClick = onRetry) {
-                    Text(text = "재시도")
+        Dialog(
+            onDismissRequest = { },
+            properties = DialogProperties(usePlatformDefaultWidth = false), // 다이얼로그 기본 Padding 제거
+        ) {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = colorResource(id = R.color.transparency_black)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .padding(start = 20.dp, end = 20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "네트워크가 끊어졌습니다.", style = TextStyle(
+                            color = colorResource(
+                                id = R.color.white
+                            ), fontSize = 16.sp, textAlign = TextAlign.Center
+                        )
+                    )
+
+                    Text(
+                        modifier = Modifier.padding(top = 5.dp),
+                        text = "네트워크 연결을 확인해 주세요.", style = TextStyle(
+                            color = colorResource(
+                                id = R.color.white
+                            ), fontSize = 16.sp, textAlign = TextAlign.Center
+                        )
+                    )
+
+                    Button(onClick = onRetry, modifier = Modifier.padding(top = 10.dp)) {
+                        Text(text = "재시도")
+                    }
                 }
             }
-        )
+
+        }
     }
 }
