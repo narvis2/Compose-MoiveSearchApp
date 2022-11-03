34e8cf2daee1c9d3d657d43d1ae06760e1d1c66d
narvis2 <narvis2@douzone.com>
2022-11-03 10:17:23 +0900

    🍎 LoadingItemView 별도의 Component 로 분리하여 관리
    ## 🍎 LoadingItemView 별도의 Component 로 분리하여 관리
    - 추후 다른 곳에서도 사용할 수 있도록 Component로 따로 분리하여 관리

---
+++ app/src/main/java/com/example/moviesearchapp/view/widgets/LoadingItemView.kt
@@ -0,0 +1,22 @@
+package com.example.moviesearchapp.view.widgets
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingItemView() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(modifier = Modifier.height(132.dp))
+        CircularProgressIndicator()
+    }
+}
